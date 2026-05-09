5a626bedd394f507c3e983ec92bc812877fda617
datlt5199-maker · he/him <datlt5199@ut.edu.vn>
2026-05-09 13:55:09 +0700

    delete page.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div>
-      Frontend Running OK 🚀
+    <div
+      style={{
+        backgroundColor: "white",
+        minHeight: "100vh",
+        color: "black",
+        padding: "20px",
+      }}
+    >
+      <h1>Frontend Running OK 🚀</h1>
+
+      <p>Trang web đã chạy thành công</p>
+
+      <button>Chạy AI</button>
     </div>
   );
 }
